51e1391065d4d3729c052beb72a696b0cccded8d
David Mwendwa <klatzdavie19@gmail.com>
2021-08-09 09:55:47 +0300

    feat: add a form with hooks

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
@@ -6,6 +6,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Grid from '@material-ui/core/Grid';
 
 import TodoList from './TodoList';
+import TodoForm from './TodoForm';
 
 const TodoApp = () => {
   const initialTodos = [
@@ -13,7 +14,12 @@ const TodoApp = () => {
     { id: 2, task: 'Wash Car', completed: true },
     { id: 3, task: 'Grow Beard', completed: false },
   ];
-  const [todos, setTodos] = useState(initialTodos)
+  const [todos, setTodos] = useState(initialTodos);
+
+  const addTodo = (newTodoText) => {
+    setTodos([...todos, { id: 4, task: newTodoText, completed: false }]);
+  };
+
   return (
     <div>
       <Paper
@@ -29,6 +35,7 @@ const TodoApp = () => {
             <Typography color='inherit'>TODOS WITH HOOOKS</Typography>
           </Toolbar>
         </AppBar>
+        <TodoForm addTodo={addTodo} />
         <TodoList todos={todos} />
       </Paper>
     </div>
