60f12a2894d183cdf0ebe723a956a6ad372cb86e
David Mwendwa <klatzdavie19@gmail.com>
2021-08-09 10:27:30 +0300

    feat: add Todo item component with material action btns

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Divider from '@material-ui/core/Divider';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
+
+import Todo from './Todo';
 
 const TodoList = (props) => {
   return (
@@ -11,15 +11,13 @@ const TodoList = (props) => {
       <List>
         {props.todos.map((todo) => (
           <>
-            <ListItem>
-              <ListItemText>{todo.task}</ListItemText>
-            </ListItem>
+            <Todo task={todo.task} key={todo.id} completed={todo.completed} />
             <Divider />
           </>
         ))}
       </List>
     </Paper>
   );
-}
+};
 
-export default TodoList
+export default TodoList;
