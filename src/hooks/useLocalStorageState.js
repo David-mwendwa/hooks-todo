43dbf8b4c65ab2919824cd33f071456befa2372f
David Mwendwa <klatzdavie19@gmail.com>
2021-08-15 06:23:57 +0300

    fix: quick fix on localStorage hook

--- src/hooks/useLocalStorageState.js
+++ src/hooks/useLocalStorageState.js
@@ -1,17 +1,18 @@
 import {useState, useEffect} from 'react';
 
 const useLocalStorageState = (key, defaultVal) => {
-  const [state, state] = useState(() => {
+  const [state, setState] = useState(() => {
     let value;
     try {
       value = JSON.parse(window.localStorage.getItem(key) || String(defaultVal))
     } catch (err) {
       value = defaultVal
     }
+    return value
   })
   useEffect(() => {
     window.localStorage.setItem(key, JSON.stringify(state))
-  }, [state])
+  }, [state, key])
   return [state, setState]
 }
 
