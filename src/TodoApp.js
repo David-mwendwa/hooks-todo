e2415d75056442e8f985d61d40db48ef6c2bc490
David Mwendwa <klatzdavie19@gmail.com>
2021-08-15 06:32:25 +0300

    feat: add in Todos context

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -5,6 +5,7 @@ import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Grid from '@material-ui/core/Grid';
+import { TodosProvider } from './context/todos.context';
 
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
@@ -36,13 +37,15 @@ const TodoApp = () => {
         </AppBar>
         <Grid container justify='center' style={{ marginTop: '1rem' }}>
           <Grid item xs={11} md={8} lg={4}>
-            <TodoForm addTodo={addTodo} />
-            <TodoList
-              todos={todos}
-              removeTodo={removeTodo}
-              toggleTodo={toggleTodo}
-              editTodo={editTodo}
-            />
+            <TodosProvider>
+              <TodoForm addTodo={addTodo} />
+              <TodoList
+                todos={todos}
+                removeTodo={removeTodo}
+                toggleTodo={toggleTodo}
+                editTodo={editTodo}
+              />
+            </TodosProvider>
           </Grid>
         </Grid>
       </Paper>
--- src/context/todos.context.js
+++ src/context/todos.context.js
@@ -1,8 +1,21 @@
 import {createContext} from 'react';
 
+import useTodoState from '../hooks/useTodoState';
+
 const defaultTodos = [
   {id: 1, task: "Mow the lawn using goats", completed: false},
   {id: 2, task: "Release lady bugs to the garden", completed: true},
 ]
 
-export const TodosContext = createContext(defaultTodos)
+export const TodosContext = createContext()
+
+export function TodosProvider(props) {
+  const todosStuff = useTodoState(defaultTodos)
+
+  return (
+    <TodosContext.Provider
+      value={todosStuff}>
+      {props.children}
+    </TodosContext.Provider>
+  );
+}
